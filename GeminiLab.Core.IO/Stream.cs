e3906d0cc0529ccb2de547cedb9fc515bd44d4f5
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2017-06-26 21:32:41 +0800

    a new func

---
+++ GeminiLab.Core.IO/Stream.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeminiLab.Core.IO
+{
+    public static class StreamExtension
+    {
+        public static IEnumerable<string> EnumerateLines(this StreamReader src)
+        {
+            while (!src.EndOfStream) yield return src.ReadLine();
+        }
+    }
+}
